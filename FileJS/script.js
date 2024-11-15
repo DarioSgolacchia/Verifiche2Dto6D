30ebbcab6f8778abcda69965d95be92311d38da4
DarioSgolacchia <dariosgolacchia@gmail.com>
2024-11-15 18:07:43 +0100

    script

--- FileJS/script.js
+++ FileJS/script.js
@@ -244,7 +244,6 @@ async function loadCSV() {
 
 // Funzione per caricare il CSV da GitHub e visualizzarlo
 document.addEventListener('DOMContentLoaded', () => {
-    loadpGIAllegatiCSV();
     loadElaboratiProgettualiCSV();
     loadClashDetectionCSV();
     loadPointCloudCSV();
@@ -261,47 +260,7 @@ document.addEventListener('DOMContentLoaded', () => {
     loadModellazioneImpiantisticaCSV();
     loadUnitaMisuraCSV();
 });
-// pGI e Allegati
-function loadpGIAllegatiCSV() {
-    const repo = 'DarioSgolacchia/Verifiche2Dto6D';
-    const filePath = 'pGIAllegati.csv';
-    const csvUrl = githubRawURL(repo, filePath);
 
-    fetch(csvUrl)
-        .then(response => {
-            if (!response.ok) throw new Error('Network response was not OK');
-            return response.text();
-        })
-        .then(data => {
-            const parsedData = Papa.parse(data, { header: false }).data;
-            const tableHeader = document.getElementById('pGIHeader');
-            const tableBody = document.getElementById('pGIBody');
-
-            if (tableHeader && tableBody) {
-                tableHeader.innerHTML = '';
-                tableBody.innerHTML = '';
-
-                parsedData[0].forEach(cell => {
-                    const th = document.createElement('th');
-                    th.textContent = cell;
-                    tableHeader.appendChild(th);
-                });
-
-                parsedData.slice(1).forEach(row => {
-                    const tr = document.createElement('tr');
-                    row.forEach(cell => {
-                        const td = document.createElement('td');
-                        td.textContent = cell;
-                        tr.appendChild(td);
-                    });
-                    tableBody.appendChild(tr);
-                });
-            } else {
-                console.error('pGIHeader o pGIBody non trovati nel DOM');
-            }
-        })
-        .catch(error => console.error('Error fetching CSV file:', error));
-}
 // Elaborati progettuali
 function loadElaboratiProgettualiCSV() {
     const repo = 'DarioSgolacchia/Verifiche2Dto6D';
