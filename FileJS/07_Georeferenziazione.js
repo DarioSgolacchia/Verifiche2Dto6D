62fbed2a111fd55ca8ac04591fc09a7cf113b78e
DarioSgolacchia <dariosgolacchia@gmail.com>
2024-11-19 10:02:24 +0100

    georeferenziazione

--- FileJS/07_Georeferenziazione.js
+++ FileJS/07_Georeferenziazione.js
@@ -1,9 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM completamente caricato.");
+    loadElaboratiGeoreferenziazioneCSV();
     loadCoordinationCSV();
 });
 
 const githubRawURL = (repo, filePath) => `https://raw.githubusercontent.com/${repo}/main/${filePath}`;
+
+// Orientamento
+function loadElaboratiGeoreferenziazioneCSV() {
+    const repo = 'DarioSgolacchia/Verifiche2Dto6D';
+    const filePath = 'FileCSV/Georeferenziazione.csv';
+    const csvUrl = githubRawURL(repo, filePath);
+
+    fetch(csvUrl)
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not OK');
+            return response.text();
+        })
+        .then(data => {
+            const parsedData = Papa.parse(data, { header: false }).data;
+            const tableHeader = document.getElementById('GeoreferenziazioneHeader');
+            const tableBody = document.getElementById('GeoreferenziazioneBody');
+
+            if (tableHeader && tableBody) {
+                tableHeader.innerHTML = '';
+                tableBody.innerHTML = '';
+
+                parsedData[0].forEach(cell => {
+                    const th = document.createElement('th');
+                    th.textContent = cell;
+                    tableHeader.appendChild(th);
+                });
+
+                parsedData.slice(1).forEach(row => {
+                    const tr = document.createElement('tr');
+                    row.forEach(cell => {
+                        const td = document.createElement('td');
+                        td.textContent = cell;
+                        tr.appendChild(td);
+                    });
+                    tableBody.appendChild(tr);
+                });
+            } else {
+                console.error('GeoreferenziazioneHeader o GeoreferenziazioneBody non trovati nel DOM');
+            }
+        })
+        .catch(error => console.error('Error fetching CSV file:', error));
+}
 
 // Georeferenziazione
 function loadCoordinationCSV() {
